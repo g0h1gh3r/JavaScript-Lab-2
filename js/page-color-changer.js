865d7a27104166f45f01f13e9965ac1461b15a40
Henry Zeng <youhengzeng8@gmail.com>
2024-10-17 14:08:37 -0400

    set the function which will update the background

--- js/page-color-changer.js
+++ js/page-color-changer.js
@@ -3,3 +3,14 @@ const redSlider = document.querySelector('#red')
 const greenSlider = document.querySelector('#green')
 const blueSlider = document.querySelector('#blue')
 const body = document.body
+
+// Add a function to update background color
+const updateBackgroundColor = () => {
+  // Get value from three slider
+  const redValue = redSlider.value
+  const greenValue = greenSlider.value
+  const blueValue = blueSlider.value
+
+  // Set the background color of body
+  body.style.backgroundColor = `rgb(${redValue},${greenValue},${blueValue})`
+}
