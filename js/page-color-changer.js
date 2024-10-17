3d521965a1b5c843a78ddd78e4118ec2ea62092e
Henry Zeng <youhengzeng8@gmail.com>
2024-10-17 14:19:39 -0400

    fix the bug that query selector invokeb before html element loaded, and finally get a null value

--- js/page-color-changer.js
+++ js/page-color-changer.js
@@ -1,21 +1,23 @@
-// Grab the element I need
-const redSlider = document.querySelector('#red')
-const greenSlider = document.querySelector('#green')
-const blueSlider = document.querySelector('#blue')
-const body = document.body
+window.addEventListener('DOMContentLoaded', () => {
+  // Grab the element I need
+  const redSlider = document.querySelector('#red')
+  const greenSlider = document.querySelector('#green')
+  const blueSlider = document.querySelector('#blue')
+  const body = document.body
 
-// Add a function to update background color
-const updateBackgroundColor = () => {
-  // Get value from three slider
-  const redValue = redSlider.value
-  const greenValue = greenSlider.value
-  const blueValue = blueSlider.value
+  // Add a function to update background color
+  const updateBackgroundColor = () => {
+    // Get value from three slider
+    const redValue = redSlider.value
+    const greenValue = greenSlider.value
+    const blueValue = blueSlider.value
 
-  // Set the background color of body
-  body.style.backgroundColor = `rgb(${redValue},${greenValue},${blueValue})`
-}
+    // Set the background color of body
+    body.style.backgroundColor = `rgb(${redValue},${greenValue},${blueValue})`
+  }
 
-//Add three event listener when user change the slider, invoke the function above
-redSlider.addEventListener('input', updateBackgroundColor)
-greenSlider.addEventListener('input', updateBackgroundColor)
-blueSlider.addEventListener('input', updateBackgroundColor)
+  //Add three event listener when user change the slider, invoke the function above
+  redSlider?.addEventListener('input', updateBackgroundColor)
+  greenSlider?.addEventListener('input', updateBackgroundColor)
+  blueSlider?.addEventListener('input', updateBackgroundColor)
+})
