8d32ce481f53095f6f93b3fc320c3dae7bb51d55
Henry Zeng <youhengzeng8@gmail.com>
2024-10-17 14:01:44 -0400

    set some constants I need

---
+++ js/page-color-changer.js
@@ -0,0 +1,5 @@
+// Grab the element I need
+const redSlider = document.querySelector('#red')
+const greenSlider = document.querySelector('#green')
+const blueSlider = document.querySelector('#blue')
+const body = document.body
